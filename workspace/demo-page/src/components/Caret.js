4254af8ee98007fe6f1b74d42d05e04e63c90d5c
Ge Yang <yangge1987@gmail.com>
2017-03-08 00:24:57 -0600

    got caret to work

--- workspace/demo-page/src/components/Caret.js
+++ workspace/demo-page/src/components/Caret.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 const style = {
-  position:"relative",
-  width: "1em",
-  marginRight: "-1em",
-  backgroundColor: "#ff5a5d",
-  height: "2em"
+  position: "relative",
+  display: "inline-block",
+  width: "5px",
+  marginRight: "-5px",
+  backgroundColor: "rgba(255, 0, 0, 0.6)",
+  height: "20px",
+  top: "5px",
+  marginTop: "calc(1em - 20px)"
 };
 export default function Caret() {
   return <span style={style}/>
