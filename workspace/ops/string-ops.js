5fe382af8f9ba6de779473d50ebe6676f21b6b84
Ge Yang <yangge1987@gmail.com>
2017-03-07 22:08:13 -0600

    change extension to standard js

--- workspace/ops/string-ops.js
+++ workspace/ops/string-ops.js
@@ -1,19 +1,11 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.ins = ins;
-exports.del = del;
-exports.mov = mov;
-function ins(s, i, v) {
+export function ins(s, i, v) {
   return s.splice(i, 0, v);
 }
-function del(i, l) {
-  return s.splice(i, l);
+export function del(i, l) {
+  return s.splice(i, l)
 }
 
-function mov(i, l, d) {
-  var seg = s.slice(i, l);
+export function mov(s, i, l, d) {
+  const seg = s.slice(i, l);
   return s.splice(i, l).splice(d, 0, seg);
 }
